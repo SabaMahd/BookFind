2cdb610bbe04294b9fedf4f49dbb8df81aec6fb5
saba mahd <sabamahd.sm@gmail.com>
2022-05-10 10:21:19 -0400

    user login+new user

--- Develop/server/utils/mutations.js
+++ Develop/server/utils/mutations.js
@@ -2,7 +2,20 @@ import { gql, useMutation } from '@apollo/client';
 
 // to add a new user
 export const ADD_USER= gql'
-mutation addUser($username: String!, $email: String!, $password: String!) {
+    mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
         token
         user {
+
+
+
+
+
+
+
+            // user log in
+ export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {    
